d4d717f9fe0959baedfef4eccd9e19342b8fd25d
Gregor Richardson <gregor.richardson@gmail.com>
2017-12-09 20:29:00 -0800

    adds javascript for interactive functionality

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-document.querySelector('.checkboxHolder').addEventListener('click', function(event){
-    // console.log(this)
-    this.querySelector('input').checked ? this.classList.add('checked') : this.classList.remove('checked');
-    // console.log(this.querySelector('input').checked)
+document.querySelector('.customStyleForm').addEventListener('click',function(e){
+    if (e.target && e.target.matches('input')) {
+        e.target.parentNode.classList.toggle('checked');
+    }
+    if (e.target && e.target.matches('.selectLabel')) {
+        e.target.parentNode.classList.toggle('open');
+    }
+});
 
-})
+document.querySelector('.customStyleForm').addEventListener('input', function(e){
+    if (e.target && e.target.matches('input.textInput')) {
+        if (e.target.validity.valid) {
+            e.target.parentNode.querySelector('.errorBox').innerHTML = '';
+            e.target.parentNode.classList.remove('error');
+        }
+        else {
+            e.target.parentNode.querySelector('.errorBox').innerHTML = "please enter a valid username";
+            e.target.parentNode.classList.add('error');
+        }        
+      }
+});
